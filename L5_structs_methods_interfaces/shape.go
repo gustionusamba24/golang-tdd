88807ba8785fe7017c6e920c8bc3b04c78dfd098
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2024-05-02 21:07:23 +0700

    refactor: add name of struct fields

--- L5_structs_methods_interfaces/shape.go
+++ L5_structs_methods_interfaces/shape.go
@@ -23,6 +23,15 @@ func (c Circle) Area() float64 {
 	return math.Pi * math.Pow(c.Radius, 2)
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2
+}
+
 func Perimeter(r Rectangle) float64 {
 	return 2 * (r.Width + r.Height)
 }
